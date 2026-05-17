f79caed6ba4ab8019dc07d51de6a23a886256959
EchosMom <zahrasalie2004@gmail.com>
2026-05-17 15:16:06 +0200

    debugging autoscript

--- AutoExperiments.sh
+++ AutoExperiments.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-PATRONS = (7 25 35 50 100 200 250)
-SCHEDULERS = (0 1 2 3 )
-RUNS = 10
+PATRONS=(7 25 35 50 100 200 250)
+SCHEDULERS=(0 1 2 3)
+RUNS=10
 
 for patrons in "${PATRONS[@]}"; do  
-    for shedulers in "${SCHEDULERS[@]}"; do
+    for schedulers in "${SCHEDULERS[@]}"; do
         for runs in $(seq 1 $RUNS); do
-            SEED = $((patrons*10000 + schedulers * 1000 + run))
+            SEED=$((patrons*10000 + schedulers * 1000 + runs))
             echo "Running: Patron num=$patrons, Scheduler=$schedulers, Run num=$runs"
-            make run ARGS = "$patrons $scheduleers 0 $SEED"
+            java -cp bin barScheduling.SchedulingSimulation $patrons $schedulers 0 $SEED
         done
     done
 done
